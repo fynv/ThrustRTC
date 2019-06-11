f9e87d438c6c569fe94737d9660825a13d813b21
fynv <feiy@nvidia.com>
2019-06-11 09:47:55 +0800

    Updating version number to 0.1.2

--- python/setup.py
+++ python/setup.py
@@ -27,7 +27,7 @@ module_PyThrustRTC = Extension(
 
 setup(
 	name = 'ThrustRTC',
-	version = '0.1.1',
+	version = '0.1.2',
 	description = 'Thrust for Python based on NVRTC',
 	long_description=long_description,
 	long_description_content_type='text/markdown',  
