38b61b3e62dfe92921438f930dc66ee1ce48743e
fynv <feiy@nvidia.com>
2019-06-21 12:46:32 +0800

    java: Adding a test script

---
+++ java/test/run_tests.bat
@@ -0,0 +1,1 @@
+java -Djava.library.path=. -cp JThrustRTCTests.jar;JThrustRTC.jar test_trtc
--- java/test/test_trtc.java
+++ java/test/test_trtc.java
@@ -5,6 +5,7 @@ public class test_trtc
 {
 	public static void main(String[] args) 
 	{
+
 	    Kernel ker = new Kernel(new String[]{ "arr_in", "arr_out", "k" }, 
 	   	String.join("",
 	  	"    size_t idx = blockIdx.x * blockDim.x + threadIdx.x;\n",
@@ -23,8 +24,7 @@ public class test_trtc
 	    DeviceViewable[] args_f = new DeviceViewable[] { dvec_in_f, dvec_out_f, k1 };
 	    ker.launch(1, 128, args_f, 0);
 
-	    float[] res = (float[])dvec_out_f.to_host();
+	    System.out.println(Arrays.toString((float[])dvec_out_f.to_host()));
 
-	    System.out.println(Arrays.toString(res));
 	}
 }
